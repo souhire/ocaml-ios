fa76299e496ad85a919a195f067e3c00e03519e0
Souhire <souhire@ocamlpro.saclay.inria.fr>
2013-09-02 15:45:39 +0200

    README + script to install it

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 ocaml-ios
 =========
+
+1. Requirements
+
+You need to build ios-clang, a toolchain based on clang for iOS that you can find on github.com/souhire/iostoolchain. 
+You can use our script to built it automatically and install it in your home directory, or modify it to install where your want.
+In order to run your OCaml programs, you need to have jailbroken iOS devices.
+
+2. Installation
+
+To install it, you just have to make git clone and once in the directory ocaml-ios run ./install.sh 
+
+3. Run
+
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DOWNLOAD=`pwd`;
+
+cd $HOME/toolchain/tests;
+mkdir 3.12.1-ios;
+wget http://caml.inria.fr/pub/distrib/ocaml-3.12/ocaml-3.12.1.tar.gz;
+tar xvf ocaml-3.12.1.tar.gz;
+cp -a ocaml-3.12.1 ocaml-3.12.1.host;
+cd ocaml-3.12.1;
+patch -p1 < $DOWNLOAD/patch.ocaml-3.12.1-ios-clang;
+
+
+cd ../ocaml-3.12.1.host;
+./configure;
+make world;
+
+
+cd ../ocaml-3.12.1;
+cp $DOWNLOAD/cross_compile_conf.arm-apple-darwin11 config/auto-aux/;
+export TOOLCHAIN=$HOME/toolchain;
+export OCAMLHOST=$TOOLCHAIN/ocaml-3.12.1.host;
+export ACC="$TOOLCHAIN/bin/ios-clang -I$TOOLCHAIN/share/iPhoneOS4.2.sdk/usr/lib/gcc/arm-apple-darwin10/4.2.1/include/ -I$TOOLCHAIN/share/iPhoneOS4.2.sdk/usr/include/ -isysroot $TOOLCHAIN/share/iPhoneOS4.2.sdk";
+./configure -verbose -prefix $TOOLCHAIN/tests/3.12.1-ios -host arm-apple-darwin11  -cc "$ACC" -as "$ACC -c" -aspp "$ACC -c" -partialld "arm-apple-darwin11-ld -r";
+make world;
+make opt;
+sudo make install;
+
+
