aef8c6684f2d501605570df68f57b8996844daf0
Souhire <souhire@ocamlpro.saclay.inria.fr>
2013-09-06 15:40:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 DOWNLOAD=`pwd`;
 
-cd $HOME/toolchain/tests;
-mkdir 3.12.1-ios;
+cd $HOME/toolchain;
+#mkdir 3.12.1-ios;
 wget http://caml.inria.fr/pub/distrib/ocaml-3.12/ocaml-3.12.1.tar.gz;
 tar xvf ocaml-3.12.1.tar.gz;
 cp -a ocaml-3.12.1 ocaml-3.12.1.host;
@@ -15,15 +15,14 @@ cd ../ocaml-3.12.1.host;
 ./configure;
 make world;
 
-
-cd ../ocaml-3.12.1;
+cd $HOME/toolchain/ocaml-3.12.1;
 cp $DOWNLOAD/cross_compile_conf.arm-apple-darwin11 config/auto-aux/;
 export TOOLCHAIN=$HOME/toolchain;
 export OCAMLHOST=$TOOLCHAIN/ocaml-3.12.1.host;
 export ACC="$TOOLCHAIN/bin/ios-clang -I$TOOLCHAIN/share/iPhoneOS4.2.sdk/usr/lib/gcc/arm-apple-darwin10/4.2.1/include/ -I$TOOLCHAIN/share/iPhoneOS4.2.sdk/usr/include/ -isysroot $TOOLCHAIN/share/iPhoneOS4.2.sdk";
-./configure -verbose -prefix $TOOLCHAIN/tests/3.12.1-ios -host arm-apple-darwin11  -cc "$ACC" -as "$ACC -c" -aspp "$ACC -c" -partialld "arm-apple-darwin11-ld -r";
+./configure -verbose -prefix $TOOLCHAIN/3.12.1-ios -host arm-apple-darwin11  -cc "$ACC" -as "$ACC -c" -aspp "$ACC -c" -partialld "arm-apple-darwin11-ld -r";
 make world;
 make opt;
-sudo make install;
+make install;
 
 
